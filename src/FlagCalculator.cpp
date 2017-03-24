7629f9e67160b27c1f775630b55fab7f06cefbf6
Lennart Nachtigall <Lennart.Nachtigall@student.kit.edu>
2017-03-24 11:40:36 +0100

    Added FlagCalculator class

---
+++ src/FlagCalculator.cpp
@@ -0,0 +1,6 @@
+#include "FlagCalculator.h"
+
+FlagCalculator::FlagCalculator()
+{
+
+}
---
+++ src/FlagCalculator.h
@@ -0,0 +1,11 @@
+#ifndef FLAGCALCULATOR_H
+#define FLAGCALCULATOR_H
+
+
+class FlagCalculator
+{
+public:
+    FlagCalculator();
+};
+
+#endif // FLAGCALCULATOR_H
