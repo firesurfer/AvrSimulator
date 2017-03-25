0ea80423000d398691733adf972dbb3f38e2760d
Lennart Nachtigall <Lennart.Nachtigall@student.kit.edu>
2017-03-25 19:16:37 +0100

    Added defines and corrected bitset methods

--- src/BitHelpers.h
+++ src/BitHelpers.h
@@ -2,22 +2,31 @@
 #define BITHELPERS_H
 #include <cinttypes>
 
+#define CLR_BIT(x,pos) BitHelpers::clear_bit(x,pos)
+#define SET_BIT(x,pos) BitHelpers::set_bit(x,pos)
+#define BIT_SET(x,pos) BitHelpers::bit_set(x,pos)
+
 class BitHelpers
 {
 public:
     BitHelpers();
-    static bool bit_set(uint8_t byte, uint8_t pos)
+    static uint8_t bit_set(uint8_t byte, uint8_t pos)
     {
-        return byte & pos;
+        return byte & (1 <<pos);
     }
-    static bool bit_set(uint16_t word, uint8_t pos)
+    static uint8_t bit_set(uint16_t word, uint8_t pos)
     {
-        return word & pos;
+        return word & (1<<pos);
     }
     static void clear_bit(uint8_t &byte, uint8_t pos)
     {
         byte = byte & ~(1<<pos);
     }
+    static void clear_bit(uint16_t &word, uint8_t pos)
+    {
+        word = word & ~(1<<pos);
+    }
+
     static void set_bit(uint8_t & byte, uint8_t pos)
     {
         byte |= (1<<pos);
