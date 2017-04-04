428f4cb2d5fcbfa48e060ebb08b54448211059ab
Lennart Nachtigall <Lennart.Nachtigall@student.kit.edu>
2017-04-04 16:28:34 +0200

    Implemented some commands - See SBIC!

--- src/Commands/SBIC.cpp
+++ src/Commands/SBIC.cpp
@@ -2,8 +2,8 @@
 
 SBIC::SBIC(MemoryMapper *_dataMemory):CommandBase(_dataMemory)
 {
-    command = 0b0001110000000000;
-    commandMask = 0b1111110000000000;
+    command = 0xb1001100100000000;
+    commandMask = 0xFF00;
     numArgs = 2;
     commandSize = 1;
     name = "SBIC";
@@ -11,35 +11,17 @@ SBIC::SBIC(MemoryMapper *_dataMemory):CommandBase(_dataMemory)
 
 uint64_t SBIC::Execute(uint16_t instruction, uint16_t &ProgramCounter)
 {
-  /*  uint8_t & sreg = SpecialRegisters[SREG];
-    uint8_t registers = (uint8_t)(instruction >> 8);
-    uint8_t regd = registers & 0xF0;
-    uint8_t regr = registers >> 4;
-    if(BitHelpers::bit_set(registers, 7))
+    uint8_t reg = (instruction & 0xF8) >> 3;
+    uint8_t bit = (instruction & 0x7);
+    if(!BIT_SET(data_memory->getIORegister(reg),bit))
     {
-        regd+=16;
+        ProgramCounter += 2;
+        return 2;
+        //TODO HOW TO CHECK IF COMMAND IS 2 WORDS
     }
-    if(BitHelpers::bit_set(registers,8))
-    {
-        regr+=16;
-    }
-    uint8_t temp = Registers[regd] + Registers[regr];
-    if(BitHelpers::bit_set(Registers[regd],3)&& BitHelpers::bit_set(Registers[regr],3)|| BitHelpers::bit_set(Registers[regr],3) && !BitHelpers::bit_set(temp,3) || !BitHelpers::bit_set(temp,3) && BitHelpers::bit_set(Registers[regd],3))
-        BitHelpers::set_bit(sreg,SREG_H);
-    if(BitHelpers::bit_set(temp,7))
-        BitHelpers::set_bit(sreg,SREG_N);
-    if(BitHelpers::bit_set(sreg, SREG_N) != BitHelpers::bit_set(sreg,SREG_V))
-        BitHelpers::set_bit(sreg,SREG_S);
-    if(temp = 0)
-        BitHelpers::set_bit(sreg,SREG_Z);
     else
-        BitHelpers::clear_bit(sreg,SREG_Z);
-    if(BitHelpers::bit_set(Registers[regd],7) && BitHelpers::bit_set(Registers[regr],7) || BitHelpers::bit_set(Registers[regr],7)  && !BitHelpers::bit_set(temp,7) || !BitHelpers::bit_set(temp,7), BitHelpers::bit_set(Registers[regd],7))
-        BitHelpers::set_bit(sreg,SREG_C);
-    if(BitHelpers::bit_set(Registers[regd],7)&& BitHelpers::bit_set(Registers[regr],7) && !BitHelpers::bit_set(temp,7) || !BitHelpers::bit_set(Registers[regd],7) || !BitHelpers::bit_set(Registers[regr],7) || BitHelpers::bit_set(temp,7))
-        BitHelpers::set_bit(sreg,SREG_V);
-
-
-    Registers[regd] = temp;*/
-    return ProgramCounter+1;
+    {
+        ProgramCounter+=1;
+        return 1;
+    }
 }
