d432c7c140d2b45073865bdbabe8084e107ddda2
Friedolin <friedolin.groeger@gmx.de>
2017-04-13 09:47:01 +0200

    implemented uart module (without interrupts)

--- src/Periphery/Uart.h
+++ src/Periphery/Uart.h
@@ -8,6 +8,10 @@ class Uart:public PeripheryElement
 public:
     Uart(MemoryMapper* mapper);
     void handle(uint32_t cycles);
+
+private:
+    void onChange(uint32_t addr, uint8_t oldval, uint8_t newval);
+    std::string buffer;
 };
 
 #endif // UART_H
