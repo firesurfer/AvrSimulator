6cc83d978ec5437df41c97e3b28b063c5d69efe6
Friedolin <friedolin.groeger@gmx.de>
2017-04-13 09:35:34 +0200

    new test with uart

---
+++ test/c/uart/uart_test.c
@@ -0,0 +1,74 @@
+#include  <avr/io.h>
+#include <avr/pgmspace.h>
+
+/* UART-Init Bsp. ATmega16 */
+#ifndef F_CPU
+#warning "F_CPU war noch nicht definiert, wird nun nachgeholt mit 4000000"
+#define F_CPU 4000000UL  // Systemtakt in Hz - Definition als unsigned long beachten 
+                         // Ohne ergeben sich unten Fehler in der Berechnung
+#endif
+
+#define BAUD 9600UL      // Baudrate
+
+// Berechnungen
+#define UBRR_VAL ((F_CPU+BAUD*8)/(BAUD*16)-1)   // clever runden
+#define BAUD_REAL (F_CPU/(16*(UBRR_VAL+1)))     // Reale Baudrate
+#define BAUD_ERROR ((BAUD_REAL*1000)/BAUD) // Fehler in Promille, 1000 = kein Fehler.
+
+void uart_init(void)
+{
+  UBRRH = UBRR_VAL >> 8;
+  UBRRL = UBRR_VAL & 0xFF;
+
+  UCSRB |= (1<<TXEN);  // UART TX einschalten
+  UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);  // Asynchron 8N1 
+}
+
+/* ATmega16 */
+int uart_putc(unsigned char c)
+{
+    while (!(UCSRA & (1<<UDRE)))  /* warten bis Senden moeglich */
+    {
+    }                             
+
+    UDR = c;                      /* sende Zeichen */
+    return 0;
+}
+
+
+/* puts ist unabhaengig vom Controllertyp */
+void uart_puts (char *s)
+{
+    while (*s)
+    {   /* so lange *s != '\0' also ungleich dem "String-Endezeichen(Terminator)" */
+        uart_putc(*s);
+        s++;
+    }
+}
+void uart_puts_p(const char *progmem_s )
+{
+    register char c;
+    
+    while ( (c = pgm_read_byte(progmem_s++)) ) 
+      uart_putc(c);
+
+}
+
+int main(void)
+{
+    volatile char x='\n';
+    uart_init();
+    uart_puts("Hallo");
+    uart_putc(' ');
+    uart_puts("Welt!\n");
+    uart_puts_p(PSTR("Hi\n"));
+    
+    uart_putc('H');
+    uart_putc('a');
+    uart_putc('l');
+    uart_putc('l');
+    uart_putc('o');
+    uart_putc(x);
+    while(1);
+    return 0;
+}
