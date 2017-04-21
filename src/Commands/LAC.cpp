b941eafd04b7f608cba3bafb7556b6f91bd6478b
Friedolin <friedolin.groeger@gmx.de>
2017-04-21 18:13:53 +0200

    fixed mask of LA[STC], return value of SWAP

--- src/Commands/LAC.cpp
+++ src/Commands/LAC.cpp
@@ -14,7 +14,7 @@ LAC::LAC(MemoryMapper *_dataMemory):CommandBase(_dataMemory)
 
 uint32_t LAC::Execute(uint16_t instruction, uint16_t &ProgramCounter, ProcessorFlags &flags)
 {
-    uint8_t reg = (instruction & ~commandMask) >> 4;
+    uint8_t reg = (instruction & 0x1F0) >> 4;
     uint8_t regData = data_memory->getRegister(reg);
     uint16_t address = data_memory->getZReg();
     uint8_t data = data_memory->getData(address);
--- src/Commands/SWAP.cpp
+++ src/Commands/SWAP.cpp
@@ -11,11 +11,12 @@ SWAP::SWAP(MemoryMapper *_dataMemory):CommandBase(_dataMemory)
 
 uint32_t SWAP::Execute(uint16_t instruction, uint16_t &ProgramCounter, ProcessorFlags &flags)
 {
-    uint8_t reg = (instruction & ~commandMask) >> 4;
+    uint8_t reg = (instruction & 0x1F0) >> 4;
     uint8_t value = data_memory->getRegister(reg);
     uint8_t lownibble = value & 0b00001111;
     uint8_t highnibble = value & 0b11110000;
     value = highnibble >> 4 | lownibble << 4;
-    data_memory->setData(reg,value);
-    return ProgramCounter+1;
+    data_memory->setRegister(reg,value);
+    ProgramCounter += 1;
+    return 1;
 }
