2f4fcad4931c7b8228d1ba3167f4e65f0f332620
Lennart Nachtigall <firesurfer127@gmail.com>
2017-04-21 18:03:15 +0200

    implemented commands

--- src/Commands/LAT.cpp
+++ src/Commands/LAT.cpp
@@ -5,8 +5,8 @@ using namespace FlagCalculator;
 
 LAT::LAT(MemoryMapper *_dataMemory):CommandBase(_dataMemory)
 {
-    command = 0b0001110000000000;
-    commandMask = 0b1111110000000000;
+    command = 0b1001000000000111;
+    commandMask = 0b1111111000001111;
     numArgs = 2;
     commandSize = 1;
     name = "LAT";
@@ -14,7 +14,13 @@ LAT::LAT(MemoryMapper *_dataMemory):CommandBase(_dataMemory)
 
 uint32_t LAT::Execute(uint16_t instruction, uint16_t &ProgramCounter, ProcessorFlags &flags)
 {
-
+    uint8_t reg = (instruction & !commandMask) >> 4;
+    uint8_t regData = data_memory->getRegister(reg);
+    uint16_t address = data_memory->getZReg();
+    uint8_t data = data_memory->getData(address);
+    data ^= regData;
+    data_memory->setData(address,data);
+    data_memory->setRegister(reg,data);
     ProgramCounter += 1;
-    return 1;
+    return 2;
 }
