98c111ebb5917ea0324f11302389f730cfe4eab4
Lennart Nachtigall <lennart.nachtigall@student.kit.edu>
2019-05-10 11:40:04 +0200

    added simple header file parser

--- src/avr/HeaderFileParser.cpp
+++ src/avr/HeaderFileParser.cpp
@@ -1,6 +1,31 @@
 #include "HeaderFileParser.h"
-
-HeaderFileParser::HeaderFileParser()
+#include <boost/filesystem.hpp>
+#include <boost/algorithm/string.hpp>
+HeaderFileParser::HeaderFileParser(std::string _file):
+    header_path(_file)
 {
 
+}
+
+void HeaderFileParser::parse()
+{
+    if(!boost::filesystem::exists(header_path))
+    {
+        throw std::runtime_error("Given headerfile does not exist");
+    }
+
+    std::ifstream file(header_path);
+    if(!file.is_open())
+        throw std::runtime_error("Could not open headerfile: " + header_path);
+
+    std::string line ="";
+    while (std::getline(file, line))
+    {
+        if(line.find("#define") != std::string::npos)
+        {
+            std::vector<std::string> splitted;
+            boost::split(splitted,line, boost::is_any_of("\t "),boost::token_compress_on);
+            defines.insert({splitted[0],splitted[1]});
+        }
+    }
 }
--- src/avr/HeaderFileParser.h
+++ src/avr/HeaderFileParser.h
@@ -1,11 +1,28 @@
 #ifndef HEADERFILEPARSER_H
 #define HEADERFILEPARSER_H
 
-
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
 class HeaderFileParser
 {
 public:
-    HeaderFileParser();
+    HeaderFileParser(std::string _file);
+
+    void parse();
+
+    template<typename T>
+    T getDefine(std::string define)
+    {
+
+    }
+private:
+    std::string header_path;
+
+
+    std::map<std::string, std::string> defines;
+
 };
 
 #endif // HEADERFILEPARSER_H
